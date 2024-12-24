01ab4de1060f43562f895093f80f9641942d335a
Elanrif <saidbacoelanrif@gmail.com>
2024-12-24 23:24:48 +0100

    feat: raccourci typeScript pour initialiser les données

--- src/app/models/FaceSnap.ts
+++ src/app/models/FaceSnap.ts
@@ -1,17 +1,8 @@
 export class FaceSnap {
-    title: string;
-    description: string;
-    createdDate: Date;
-    snaps: number;
-    imageUrl: string;
-
-    constructor(title: string, description: string, imageUrl:string,
-        createdDate: Date, snaps: number) {
-            this.title = title;
-            this.description = description;
-            this.imageUrl = imageUrl;
-            this.createdDate = createdDate;
-            this.snaps = snaps;
-        }
+    constructor(public title: string,
+                description: string, 
+                mageUrl:string,
+                createdDate: Date, 
+                snaps: number) {}
     
 }
