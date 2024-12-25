618946f595aa3c5ca2ad6ce04f9b6f997b6ecff2
Elanrif <saidbacoelanrif@gmail.com>
2024-12-25 15:10:01 +0100

    feat: old way touse @for to loop component data, because of that we have to change logic to our component. or a component handle one simple thing

--- src/app/models/FaceSnap.ts
+++ src/app/models/FaceSnap.ts
@@ -1,5 +1,7 @@
 export class FaceSnap {
  
+  location?: string;
+
     constructor(
     public title: string,
     public description: string,
@@ -14,5 +16,9 @@ export class FaceSnap {
 
   removeSnap(): void {
     this.snaps --;
+  }
+
+  setLocation(location: string): void {
+    this.location = location;
   }
 }
