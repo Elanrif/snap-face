7b9bdaece88dc66624970a8cf864d6a28d2541d9
Elanrif <saidbacoelanrif@gmail.com>
2024-12-25 15:25:10 +0100

    feat: ngStyle

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/FaceSnap';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
