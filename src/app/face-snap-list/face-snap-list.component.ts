682e0cd07073d72c9d71c329e1578649951ba4e6
Elanrif <saidbacoelanrif@gmail.com>
2024-12-26 16:14:05 +0100

    feat: cette approche nous permet lors d une instantiation de configurer un objet

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnap } from '../models/FaceSnap';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,31 +11,11 @@ import { FaceSnap } from '../models/FaceSnap';
 })
 export class FaceSnapListComponent implements OnInit{
   faceSnaps!: FaceSnap[];
+  //Dependency Ijection with constructor
+  constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      new FaceSnap(
-        'Archibald',
-        'Mon meilleur ami depuis toujours !',
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        new Date(),
-        10
-      ),
-      new FaceSnap(
-        'Three Rock Mountain',
-        'Un endroit magnifique pour les randonnées.',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        new Date(),
-        6
-      ),
-      new FaceSnap(
-        'Un bon repas',
-        "Mmmh que c'est bon !",
-        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        new Date(),
-        156
-      ),
-    ];
+    this.faceSnaps = this.faceSnapsService.getFaceSnaps();
     this.faceSnaps[1].setLocation('à la montagne');
   }
 }
